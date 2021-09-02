99ae36d2901f8a50229688e028d818d037f8829c
boundsounds <76763933+boundsounds@users.noreply.github.com>
2021-09-01 19:31:55 -0600

    fix link color in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,17 +9,20 @@ class Footer extends Component {
           <div className="font-color">
             <h1 className="grey-text text-lighten-4">Follow Me !</h1>
 
-            <a href="https://www.linkedin.com/in/granthood21/">
+            <a
+              className="font-color"
+              href="https://www.linkedin.com/in/granthood21/"
+            >
               Linkedin{" "}
               <img src="https://img.icons8.com/ios/50/000000/linkedin.png" />
             </a>
 
-            <a href="https://www.granthood.org/home">
+            <a className="font-color" href="https://www.granthood.org/home">
               Website{" "}
               <img src="https://img.icons8.com/ios/50/000000/webpage.png" />
             </a>
 
-            <a href="https://www.granthood.org/home">
+            <a className="font-color" href="https://www.granthood.org/home">
               GitHub{" "}
               <img src="https://img.icons8.com/ios/50/000000/github--v1.png" />
             </a>
